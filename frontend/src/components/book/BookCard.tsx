9227e7f5556bd4303a97ecb57f64f563f6ad493e
Anton Podkur <anton.podkur@gmail.com>
2024-02-04 00:37:51 +0200

    Fixed editor placeholder positioning

--- frontend/src/components/book/BookCard.tsx
+++ frontend/src/components/book/BookCard.tsx
@@ -32,7 +32,7 @@ const BookCard: React.FC<BookCardProps> = ({book}) => {
   return (
     <div className={"w-full h-full flex flex-col items-center justify-center border-2 rounded-lg"}>
       <Link to={`/book/edit/${book.id}`} className={"w-full h-full flex flex-col items-center justify-center"}>
-        <div className="flex items-center justify-center w-10/12 h-32 mt-4 mb-2 bg-slate-100 font-bold text-xl border-2 rounded-lg">
+        <div className="flex items-center justify-center text-center w-10/12 h-32 mt-4 mb-2 bg-slate-100 font-bold text-xl border-2 rounded-lg">
           <div>{ book.title }</div>
         </div>
         <div className="m-2 font-semibold">{ book.author }</div>
